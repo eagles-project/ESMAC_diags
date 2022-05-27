56b6894f7bef7971a5f1a6dbb8f97df927da49c1
Shuaiqi Tang <tang32@llnl.gov>
2022-05-26 17:17:08 -0700

    add a tutorial presentation file. update setup files

--- setup.py
+++ setup.py
@@ -1,2 +1,18 @@
-import setuptools
-setuptools.setup()
+from setuptools import setup, find_packages
+
+setup(
+        name="esmac_diags",
+        version = "2.0.0",
+        license="BSD",
+        packages=find_packages(
+            where='src'
+            ),
+        package_dir={"": "src"},
+        install_requires=[
+            "numpy",
+            "pytest",
+            "matplotlib",
+            "netCDF4",
+            "pip"]
+        )
+
