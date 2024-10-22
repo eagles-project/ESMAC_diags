962fef8cada178c31ace3b3065c2bd59c0db5fea
avarble <adam.varble@pnnl.gov>
2024-10-22 16:20:48 -0600

    Update prep_CSET_E3SM.py

--- scripts/prep_CSET_E3SM.py
+++ scripts/prep_CSET_E3SM.py
@@ -13,13 +13,21 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% set input data path
-input_path = '../raw_data/model/'
+# Load configuration file
+config_file = sys.argv[1]
+stream = open(config_file, "r")
+config = yaml.full_load(stream)
+
+input_path = config['model_input_path']
+input_filehead = config['model_input_filehead']
+# input_path = '../raw_data/model/'
+# input_filehead = 'E3SMv2_CSET'
 output_path = '../prep_data/CSET/model/'
-input_filehead = 'E3SMv2_CSET'
 output_filehead = 'E3SMv2_CSET'
 
 # flight data path
-RFpath = '../raw_data/obs/CSET/aircraft/aircraft_lowrate/'
+obs_input_path = config['obs_input_path']
+RFpath = obs_input_path + 'CSET/aircraft/aircraft_lowrate/'
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 prep.prep_E3SM_flight(input_path, input_filehead, output_path, output_filehead, RFpath, dt=60)
