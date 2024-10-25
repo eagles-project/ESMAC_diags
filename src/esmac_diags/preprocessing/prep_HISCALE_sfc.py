46a651889388ab26f5d4fd020b501e190d21d8a5
avarble <adam.varble@pnnl.gov>
2024-10-25 17:04:34 -0600

    Update prep_HISCALE_sfc.py

--- src/esmac_diags/preprocessing/prep_HISCALE_sfc.py
+++ src/esmac_diags/preprocessing/prep_HISCALE_sfc.py
@@ -1118,12 +1118,17 @@ def prep_LTS(armbepath, predatapath, dt=3600):
     # time_new = pd.date_range(start=startdate, end=enddate, freq=str(int(dt))+"s")
     time_new = pd.date_range(start='2016-04-25', end='2016-09-23', freq=str(int(dt))+"s")  # HISCALE time period
 
+    tmpLTS700 = xr.DataArray(data=np.array(LTS700_valid), dims=["time"], coords=dict(time=time700_valid))
+    tmpLTS850 = xr.DataArray(data=np.array(LTS850_valid), dims=["time"], coords=dict(time=time850_valid))
+    
     if dt >= 3600:
         LTS700_new = avg_time_1d(time700_valid, LTS700_valid, time_new)
         LTS850_new = avg_time_1d(time850_valid, LTS850_valid, time_new)
     if dt < 3600:
-        LTS700_new = interp_time_1d(time700_valid, LTS700_valid, time_new)
-        LTS850_new = interp_time_1d(time850_valid, LTS850_valid, time_new)
+        # LTS700_new = interp_time_1d(time700_valid.astype("float"), LTS700_valid, time_new.astype("float"))
+        # LTS850_new = interp_time_1d(time850_valid.astype("float"), LTS850_valid, time_new.astype("float"))
+        LTS700_new = interp_time_1d(time700_valid, tmpLTS700, time_new, arraytype='xarray')
+        LTS850_new = interp_time_1d(time850_valid, tmpLTS850, time_new, arraytype='xarray')
         
     #%% output file
     outfile = predatapath + 'LTS_HISCALE.nc'
