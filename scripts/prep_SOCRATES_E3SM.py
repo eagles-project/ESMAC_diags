dd51140adceabf6e7a8791a96b8a6511c1426c15
avarble <adam.varble@pnnl.gov>
2024-11-03 12:57:14 -0700

    Update prep_SOCRATES_E3SM.py

--- scripts/prep_SOCRATES_E3SM.py
+++ scripts/prep_SOCRATES_E3SM.py
@@ -6,6 +6,9 @@ inlcude:
     prep_SOCRATES_E3SM
 """
 
+import os
+import sys
+import yaml
 import esmac_diags
 import esmac_diags.preprocessing.prep_SOCRATES_E3SM as prep
 
@@ -13,13 +16,24 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% set input data path
-input_path = '../raw_data/model/'
+# Load configuration file
+config_file = sys.argv[1]
+stream = open(config_file, "r")
+config = yaml.full_load(stream)
+
+input_path = config['model_input_path']
+input_filehead = config['model_input_filehead']
+# input_path = '../raw_data/model/'
+# input_filehead = 'E3SMv2_SO'
 output_path = '../prep_data/SOCRATES/model/'
-input_filehead = 'E3SMv2_SO'
 output_filehead = 'E3SMv2_SOCRATES'
 
 # flight data path
-RFpath = '../raw_data/obs/SOCRATES/aircraft/aircraft_lowrate/'
+obs_input_path = config['obs_input_path']
+RFpath = obs_input_path + 'SOCRATES/aircraft/aircraft_lowrate/'
+
+# time frequencies
+aircraft_dt = config['model_aircraft_dt']
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-prep.prep_E3SM_flight(input_path, input_filehead, output_path, output_filehead, RFpath, dt=60)
+prep.prep_E3SM_flight(input_path, input_filehead, output_path, output_filehead, RFpath, dt=aircraft_dt)
