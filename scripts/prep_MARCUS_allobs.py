89dda33456dabdede6edc49907d2390ee3e9648d
avarble <adam.varble@pnnl.gov>
2024-11-03 12:38:54 -0700

    Update prep_MARCUS_allobs.py

--- scripts/prep_MARCUS_allobs.py
+++ scripts/prep_MARCUS_allobs.py
@@ -5,33 +5,47 @@ inlcude:
     prep_MARCUS_ship
 """
 
+import os
+import sys
+import yaml
 import esmac_diags.preprocessing.prep_MARCUS_ship as ship
 import esmac_diags.preprocessing.prep_MARCUS_satellite as sat
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% set input data path
+# Load configuration file
+config_file = sys.argv[1]
+stream = open(config_file, "r")
+config = yaml.full_load(stream)
+
+# data path for obs
+obs_input_path = config['obs_input_path']
+
 # ship data path
-shipmetpath = '../raw_data/obs/MARCUS/ship/maraadmetX1.b1/'
-mwrpath = '../raw_data/obs/MARCUS/ship/marmwrret1liljclouM1.s2/'
-cpcpath = '../raw_data/obs/MARCUS/ship/maraoscpcf1mM1.b1/'
-ccnpath = '../raw_data/obs/MARCUS/ship/maraosccn1colavgM1.b1/'
-uhsaspath = '../raw_data/obs/MARCUS/ship/maraosuhsasM1.a1/'
-exhaustfreepath = '../raw_data/obs/MARCUS/ship/ship_exhaustfree/'
-visstgridpath = '../raw_data/obs/MARCUS/visst/grid/'
+shipmetpath = obs_input_path+'MARCUS/ship/maraadmetX1.b1/'
+mwrpath = obs_input_path+'MARCUS/ship/marmwrret1liljclouM1.s2/'
+cpcpath = obs_input_path+'MARCUS/ship/maraoscpcf1mM1.b1/'
+ccnpath = obs_input_path+'MARCUS/ship/maraosccn1colavgM1.b1/'
+uhsaspath = obs_input_path+'MARCUS/ship/maraosuhsasM1.a1/'
+exhaustfreepath = obs_input_path+'MARCUS/ship/ship_exhaustfree/'
+visstgridpath = obs_input_path+'MARCUS/visst/grid/'
 
 # output data path
 prep_data_path = '../prep_data/MARCUS/'
 
+# time frequencies
+surface_dt = config['obs_surface_dt']
+satellite_dt = config['obs_satellite_dt']
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 # prepare ship data. output time in 1hr (dt=3600s) resolution
 print('prepare ship data:')
-# ship.prep_CCN(shipmetpath, ccnpath, prep_data_path+'ship/', dt=3600)              # CCN number concentration
-# ship.prep_CN(shipmetpath, cpcpath, uhsaspath, prep_data_path+'ship/', dt=3600)    # aerosol number concentration
-# ship.prep_CNsize(shipmetpath, uhsaspath, prep_data_path+'ship/', dt=3600)   # aerosol size distribution from UHSAS
-# ship.prep_CCN_exhaustfree(shipmetpath, exhaustfreepath, prep_data_path+'ship/', dt=3600)              # CCN number concentration
-# ship.prep_CN_exhaustfree(shipmetpath, exhaustfreepath, prep_data_path+'ship/', dt=3600)    # aerosol number concentration
-# ship.prep_CNsize_exhaustfree(shipmetpath, exhaustfreepath, prep_data_path+'ship/', dt=3600)   # aerosol size distribution from UHSAS
-# ship.prep_MWR(shipmetpath, mwrpath, prep_data_path+'ship/', dt=3600) # cloud liquid water path
+# ship.prep_CCN(shipmetpath, ccnpath, prep_data_path+'ship/', dt=surface_dt)              # CCN number concentration
+# ship.prep_CN(shipmetpath, cpcpath, uhsaspath, prep_data_path+'ship/', dt=surface_dt)    # aerosol number concentration
+# ship.prep_CNsize(shipmetpath, uhsaspath, prep_data_path+'ship/', dt=surface_dt)   # aerosol size distribution from UHSAS
+# ship.prep_CCN_exhaustfree(shipmetpath, exhaustfreepath, prep_data_path+'ship/', dt=surface_dt)              # CCN number concentration
+# ship.prep_CN_exhaustfree(shipmetpath, exhaustfreepath, prep_data_path+'ship/', dt=surface_dt)    # aerosol number concentration
+# ship.prep_CNsize_exhaustfree(shipmetpath, exhaustfreepath, prep_data_path+'ship/', dt=surface_dt)   # aerosol size distribution from UHSAS
+# ship.prep_MWR(shipmetpath, mwrpath, prep_data_path+'ship/', dt=surface_dt) # cloud liquid water path
 
 print('prepare satellite data:')
-sat.prep_VISST_grid(shipmetpath, visstgridpath, prep_data_path+'satellite/', dt=3600)              # CCN number concentration
+sat.prep_VISST_grid(shipmetpath, visstgridpath, prep_data_path+'satellite/', dt=satellite_dt)              # CCN number concentration
