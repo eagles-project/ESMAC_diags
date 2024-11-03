034b586abe362a05e522a97f41d7997f7976f28d
avarble <adam.varble@pnnl.gov>
2024-11-03 12:42:13 -0700

    Update prep_CSET_allobs.py

--- scripts/prep_CSET_allobs.py
+++ scripts/prep_CSET_allobs.py
@@ -6,22 +6,34 @@ inlcude:
     prep_CSET_flight
 """
 
-import numpy as np
+import os
+import sys
+import yaml
 import esmac_diags
 import esmac_diags.preprocessing.prep_CSET_flight as air
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% set input data path
+# Load configuration file
+config_file = sys.argv[1]
+stream = open(config_file, "r")
+config = yaml.full_load(stream)
+
+# data path for obs
+obs_input_path = config['obs_input_path']
+
 # flight data path
-RFpath = '/global/cscratch1/sd/sqtang/EAGLES/ESMAC_Diags_v2/raw_data/obs/CSET/aircraft/aircraft_lowrate/'
+RFpath = obs_input_path+'CSET/aircraft/aircraft_lowrate/'
 
 # output data path
 prep_data_path = '/global/cscratch1/sd/sqtang/EAGLES/ESMAC_Diags_v2/prep_data/CSET/'
 
+# time frequencies
+aircraft_dt = config['obs_aircraft_dt']
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 # prepare flight data; output time in 1min (dt=60s) resolution
 print('prepare aircraft data:')
-air.prep_CNsize(RFpath, prep_data_path+'flight/', dt=60)    # aerosol size distribution
-air.prep_CN(RFpath, prep_data_path+'flight/', dt=60)        # CN number concentration
-air.prep_LWC(RFpath, prep_data_path+'flight/', dt=60)       # cloud liquid water content
-air.prep_Nd(RFpath, prep_data_path+'flight/', dt=60)        # cloud size distribution
+air.prep_CNsize(RFpath, prep_data_path+'flight/', dt=aircraft_dt)    # aerosol size distribution
+air.prep_CN(RFpath, prep_data_path+'flight/', dt=aircraft_dt)        # CN number concentration
+air.prep_LWC(RFpath, prep_data_path+'flight/', dt=aircraft_dt)       # cloud liquid water content
+air.prep_Nd(RFpath, prep_data_path+'flight/', dt=aircraft_dt)        # cloud size distribution
