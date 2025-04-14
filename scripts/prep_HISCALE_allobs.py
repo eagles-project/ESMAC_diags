34d7b41c2c308dfcb77db9605da9975b8ad161bb
Adam Varble <adam.varble@pnnl.gov>
2025-04-14 12:54:47 -0600

    Update prep_HISCALE_allobs.py

--- scripts/prep_HISCALE_allobs.py
+++ scripts/prep_HISCALE_allobs.py
@@ -83,10 +83,10 @@ height_out = np.array([0.,50,100,150,200,250,300,350,400,450,500,600,700,800,900
 print('prepare surface data:')
 # sfc.prep_ACSM(acsmpath, prep_data_path+'surface/', dt=surface_dt)            # aerosol composition
 # sfc.prep_ccn(ccnsfcpath, prep_data_path+'surface/', dt=surface_dt)              # CCN number concentration
-sfc.prep_CPC(cpcpath, cpcupath, prep_data_path+'surface/', dt=surface_dt)    # aerosol number concentration (>3 or 10nm)
-# sfc.prep_CNsize_UHSAS(uhsaspath, prep_data_path+'surface/', dt=surface_dt)   # aerosol size distribution from UHSAS
-# sfc.prep_CNsize_SMPS_IOP1(smpspath, nanosmpspath, prep_data_path+'surface/', dt=surface_dt)# aerosol size distribution from SMPS for IOP1
-# sfc.prep_CNsize_SMPS_IOP2(smps_pnnl_path, prep_data_path+'surface/', dt=surface_dt)# aerosol size distribution from SMPS for IOP2
+# sfc.prep_CPC(cpcpath, cpcupath, prep_data_path+'surface/', dt=surface_dt)    # aerosol number concentration (>3 or 10nm)
+sfc.prep_CNsize_UHSAS(uhsaspath, prep_data_path+'surface/', dt=surface_dt)   # aerosol size distribution from UHSAS
+sfc.prep_CNsize_SMPS_IOP1(smpspath, nanosmpspath, prep_data_path+'surface/', dt=surface_dt)# aerosol size distribution from SMPS for IOP1
+sfc.prep_CNsize_SMPS_IOP2(smps_pnnl_path, prep_data_path+'surface/', dt=surface_dt)# aerosol size distribution from SMPS for IOP2
 # sfc.prep_cloud_2d(armbepath, prep_data_path+'surface/', height_out, dt=surface_dt)   # 2D cloud fraction
 # sfc.prep_cloudheight_ARSCL(arsclpath, prep_data_path+'surface/', dt=surface_dt)   # cloud height 
 # sfc.prep_totcld(armbepath, prep_data_path+'surface/', dt=surface_dt)         # cloud fraction. from ARSCL, TSI and satellite sources
