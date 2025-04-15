1cad4ebf25f22f2d86f61fe882e580f7e8718e1e
Adam Varble <adam.varble@pnnl.gov>
2025-04-15 12:23:25 -0600

    Update prep_HISCALE_allobs.py

--- scripts/prep_HISCALE_allobs.py
+++ scripts/prep_HISCALE_allobs.py
@@ -98,8 +98,8 @@ print('prepare surface data:')
 # sfc.prep_totcld(armbepath, arsclbndpath, tsipath, prep_data_path+'surface/', dt=surface_dt)         # cloud fraction. from ARSCL, TSI and satellite sources
 # sfc.prep_LWP(armbepath, mwrpath, prep_data_path+'surface/', dt=surface_dt) # cloud liquid water path
 # sfc.prep_Ndrop(ndroppath, prep_data_path+'surface/', dt=surface_dt)          # cloud droplet number retrieval from ARM Ndrop VAP
-sfc.prep_mfrsr_cod(mfrsrpath,  prep_data_path+'surface/', dt=surface_dt)     # cloud optical depth from MFRSR
-sfc.prep_mfrsr_Reff(mfrsrpath,  prep_data_path+'surface/', dt=surface_dt)    # cloud effective radius from MFRSR
+# sfc.prep_mfrsr_cod(mfrsrpath,  prep_data_path+'surface/', dt=surface_dt)     # cloud optical depth from MFRSR
+# sfc.prep_mfrsr_Reff(mfrsrpath,  prep_data_path+'surface/', dt=surface_dt)    # cloud effective radius from MFRSR
 sfc.prep_radiation(armbepath, radfluxpath, prep_data_path+'surface/', dt=surface_dt)      # surface radiation
 sfc.prep_LTS(armbepath, prep_data_path+'surface/', dt=surface_dt)            # lower tropospheric stability
 sfc.prep_precip(armbepath, metpath, parspath, prep_data_path+'surface/', dt=surface_dt)         # surface precipitation
