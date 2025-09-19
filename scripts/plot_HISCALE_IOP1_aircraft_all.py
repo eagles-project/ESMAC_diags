c7f0573b0dd3cbf6f2ee972c6c3191fda771947e
Adam Varble <adam.varble@pnnl.gov>
2025-09-19 13:40:28 -0600

    Update plot_HISCALE_IOP1_aircraft_all.py

--- scripts/plot_HISCALE_IOP1_aircraft_all.py
+++ scripts/plot_HISCALE_IOP1_aircraft_all.py
@@ -127,7 +127,7 @@ if config['aerosol_output'] == True:
     soa_m = modeldata['soa'].load().where(np.logical_and(modeldata.time>time1, modeldata.time<time2), drop=True)
 if config['ccn_output'] == True:
     # ccn1_m = modeldata['CCN3'].load().where(np.logical_and(modeldata.time>time1, modeldata.time<time2), drop=True)
-    ccn2_m = modeldata['CCN3'].load().where(np.logical_and(modeldata.time>time1, modeldata.time<time2), drop=True)
+    ccn2_m = modeldata['CCN2'].load().where(np.logical_and(modeldata.time>time1, modeldata.time<time2), drop=True)
     ccn5_m = modeldata['CCN5'].load().where(np.logical_and(modeldata.time>time1, modeldata.time<time2), drop=True)    
 lwc_m = modeldata['cwc'].load().where(np.logical_and(modeldata.time>time1, modeldata.time<time2), drop=True)
 if config['reff_output'] == True:
@@ -559,5 +559,6 @@ if config['reff_output'] == True:
                         xlabel='Nd (cm$^{-3}$)', ylabel='Reff ($\mu$m)', 
                         linear_fit=False, intercept=True)
     fig.savefig(figpath+'scatter_Reff_Nd_'+site+'_'+IOP+'.png',dpi=fig.dpi,bbox_inches='tight', pad_inches=1)
+
 
 
