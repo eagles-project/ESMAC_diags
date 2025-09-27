2855ec583e36219751cce16a9377f6b9195d4afa
Adam Varble <adam.varble@pnnl.gov>
2025-09-27 16:05:30 -0600

    Update prep_HISCALE_allobs.py

--- scripts/prep_HISCALE_allobs.py
+++ scripts/prep_HISCALE_allobs.py
@@ -88,7 +88,7 @@ height_out = np.array([0.,50,100,150,200,250,300,350,400,450,500,600,700,800,900
 # prepare surface data. output time in 1hr (dt=3600s) resolution
 print('prepare surface data:')
 # sfc.prep_ACSM(acsmpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)            # aerosol composition
-# sfc.prep_ccn(ccnsfcpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)              # CCN number concentration
+sfc.prep_ccn(ccnsfcpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)              # CCN number concentration
 # sfc.prep_CPC(cpcpath, cpcupath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)    # aerosol number concentration (>3 or 10nm)
 # sfc.prep_CNsize_UHSAS(uhsaspath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)   # aerosol size distribution from UHSAS
 # sfc.prep_CNsize_SMPS_IOP1(smpspath, nanosmpspath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)# aerosol size distribution from SMPS for IOP1
@@ -100,10 +100,11 @@ print('prepare surface data:')
 # sfc.prep_Ndrop(ndroppath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)          # cloud droplet number retrieval from ARM Ndrop VAP
 # sfc.prep_mfrsr_cod(mfrsrpath,  prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)     # cloud optical depth from MFRSR
 # sfc.prep_mfrsr_Reff(mfrsrpath,  prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)    # cloud effective radius from MFRSR
-sfc.prep_radiation(armbepath, radfluxpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)      # surface radiation
+# sfc.prep_radiation(armbepath, radfluxpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)      # surface radiation
 # sfc.prep_LTS(armbepath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)            # lower tropospheric stability
 # sfc.prep_precip(armbepath, metpath, parspath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)         # surface precipitation
 # prepare satellite data. output time in 1hr (dt=3600s) resolution
 # print('prepare satellite data:')
 # sat.prep_VISST_grid(visstgridpath, prep_data_path+'satellite/'+str(satellite_dt)+'s/', dt=satellite_dt)     # VISST 0.5x0.5 degree gridded data
 # sat.prep_VISST_pixel(visstpixpath, prep_data_path+'satellite/'+str(satellite_dt)+'s/', dt=satellite_dt)     # VISST 4km pixel-level data
+
