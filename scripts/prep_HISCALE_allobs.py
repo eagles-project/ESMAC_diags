9c994b2940f774675efacf587636ccc88f52c1c3
Adam Varble <adam.varble@pnnl.gov>
2025-10-08 15:02:06 -0600

    Update prep_HISCALE_allobs.py

--- scripts/prep_HISCALE_allobs.py
+++ scripts/prep_HISCALE_allobs.py
@@ -86,7 +86,7 @@ height_out = np.array([0.,50,100,150,200,250,300,350,400,450,500,600,700,800,900
 # air.prep_mergeSD(mergeSDpath, iwgpath, prep_data_path+'flight/'+str(aircraft_dt)+'s/', dt=aircraft_dt) # merged cloud size distribution
 # air.prep_WCM(wcmpath, iwgpath, prep_data_path+'flight/'+str(aircraft_dt)+'s/', dt=aircraft_dt)       # cloud liquid water content
 # prepare surface data. output time in 1hr (dt=3600s) resolution
-print('prepare surface data:')
+# print('prepare surface data:')
 # sfc.prep_ACSM(acsmpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)            # aerosol composition
 # sfc.prep_ccn(ccnsfcpath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)              # CCN number concentration
 # sfc.prep_CPC(cpcpath, cpcupath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)    # aerosol number concentration (>3 or 10nm)
@@ -104,9 +104,10 @@ print('prepare surface data:')
 # sfc.prep_LTS(armbepath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)            # lower tropospheric stability
 # sfc.prep_precip(armbepath, metpath, parspath, prep_data_path+'surface/'+str(surface_dt)+'s/', dt=surface_dt)         # surface precipitation
 # prepare satellite data. output time in 1hr (dt=3600s) resolution
-# print('prepare satellite data:')
+print('prepare satellite data:')
 sat.prep_VISST_grid(visstgridpath, prep_data_path+'satellite/'+str(satellite_dt)+'s/', dx=0.5, dt=satellite_dt)     # VISST 0.5x0.5 degree gridded data
 sat.prep_VISST_pixel(visstpixpath, prep_data_path+'satellite/'+str(satellite_dt)+'s/', dx=4, dt=satellite_dt)     # VISST 4km pixel-level data
+
 
 
 
