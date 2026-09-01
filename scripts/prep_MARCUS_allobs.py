ec91a6517a1823e40dcfb71a7a9b86c74cbc5334
Adam Varble <adam.varble@pnnl.gov>
2026-09-01 11:37:43 -0600

    Add spatial resolution keyword to VISST function calls

--- scripts/prep_MARCUS_allobs.py
+++ scripts/prep_MARCUS_allobs.py
@@ -48,4 +48,4 @@ print('prepare ship data:')
 # ship.prep_MWR(shipmetpath, mwrpath, prep_data_path+'ship/', dt=surface_dt) # cloud liquid water path
 
 print('prepare satellite data:')
-sat.prep_VISST_grid(shipmetpath, visstgridpath, prep_data_path+'satellite/', dt=satellite_dt)              # CCN number concentration
+sat.prep_VISST_grid(shipmetpath, visstgridpath, prep_data_path+'satellite/', dx=0.5, dt=satellite_dt)
