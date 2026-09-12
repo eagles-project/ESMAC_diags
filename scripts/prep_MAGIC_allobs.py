3ec4484b4062ccced7cb2202cbab195d72835fc7
Adam Varble <adam.varble@pnnl.gov>
2026-09-11 19:16:35 -0600

    Update prep_MAGIC_allobs.py

--- scripts/prep_MAGIC_allobs.py
+++ scripts/prep_MAGIC_allobs.py
@@ -41,8 +41,8 @@ satellite_dt = config['obs_satellite_dt']
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 # prepare ship data. output time in 1hr (dt=3600s) resolution
 print('prepare ship data:')
-ship.prep_CCN(shipmetpath, ccnpath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt)              # CCN number concentration
-ship.prep_CN(shipmetpath, cpcpath, uhsaspath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt)    # aerosol number concentration (>3 or 10nm)
+# ship.prep_CCN(shipmetpath, ccnpath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt)              # CCN number concentration
+# ship.prep_CN(shipmetpath, cpcpath, uhsaspath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt)    # aerosol number concentration (>3 or 10nm)
 ship.prep_CNsize(shipmetpath, uhsaspath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt)   # aerosol size distribution from UHSAS
 ship.prep_MWR(shipmetpath, mwrpath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt) # cloud liquid water path
 ship.prep_Nd_Wu_etal(Ndpath, prep_data_path+'ship/'+str(surface_dt)+'s/', dt=surface_dt)          # cloud droplet number retrieval from Wu et al.
